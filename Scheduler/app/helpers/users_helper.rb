49d4ca86cfb463ca25777e0f8358133432bfa699
“Bryan <bryan87reed@gmail.com>
2017-09-14 12:50:15 -0700

    Adds logic from View to UsersHelper
    method get_start_end_time_slots(@slot) in helper

--- Scheduler/app/helpers/users_helper.rb
+++ Scheduler/app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+    def get_start_end_time_slots(slot)
+        string = String.new
+            slot.each do |key, value|
+                string << "Your #{key} times are #{value}. "
+            end 
+        return string
+    end 
+
 end
