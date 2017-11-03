6d02c70c4867c3e20a27d6a45ba8f64bdbde0769
“Bryan <bryan87reed@gmail.com>
2017-11-03 09:02:17 -0700

    Testing iCal invite

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,7 @@ class UserMailer < ApplicationMailer
 
     def invite_email(user, requestor)
         @user = user
+        attachments['sample.ics'] = File.read("tmp/ics_files/sample.ics")
         mail(to: requestor, subject: 'Invitation for Scheduled Appointment')
     end
 
