29a8cf9b54793ce632a56d64ebd69ae3381d0f4c
“Bryan <bryan87reed@gmail.com>
2017-11-02 17:33:03 -0700

    Adds Send invite to self button to datepicker
    Still no .ICS file created & attached

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   
-  get "/:username", to: 'users#datepicker' #for requester to browse to datepicker
+  get "/:username", to: 'users#datepicker', as: 'datepicker' #for requester to browse to datepicker
   post :scheduled, to: 'users#scheduled', as: :scheduled
   post "/users/:id", to: 'users#show', as: 'showme' #lets integration form post to page for api call
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
