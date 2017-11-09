5b4b560053bc5ea5bce30d1d4755932b8d9289ec
Jere Rutter <jere@redtailtechnology.com>
2017-11-09 15:29:13 -0800

    Code review 11/3

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,4 @@
-module UsersHelper 
+module UsersHelper
     # Creates timepicker html element
     def timepicker
         content_tag(:input, nil, class: "timepicker", size: '10')
@@ -7,13 +7,13 @@ module UsersHelper
     def available_time_slots_for(time_hash, day)
         if time_hash == nil
             return
-        else 
+        else
             time_hash = JSON.parse(time_hash)
             html = String.new
             time_hash['value'] = time_hash['value'].select {|time_slot| time_slot['day'].include?(day) }
             time_hash['value'].each do |index|
                 html <<  content_tag(:div, class: 'slotbox', data: {value: index, order: index['data']['start'], day: index['day']} ) do
-                    content_tag(:span, index['show'], class: 'timespan') +  
+                    content_tag(:span, index['show'], class: 'timespan') +
                     content_tag(:button, 'x', class:'remove')
                     end
              end
@@ -33,7 +33,7 @@ module UsersHelper
           idArray.push(i)
          html << '<tr>
           <td colspan="8">
-          <div> <button value=' + butVal + ' class="timeSlot" data-day='  + array['day'] + ' data-length='+ array['length'].to_s + ' id=' + idArray[i].to_s + '>' +  array['show'] +  ' </button> 
+          <div> <button value=' + butVal + ' class="timeSlot" data-day='  + array['day'] + ' data-length='+ array['length'].to_s + ' id=' + idArray[i].to_s + '>' +  array['show'] +  ' </button>
           </div>
           </td>
           </tr>'
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users do
+    member do
+      post 'link_to_redtail'
+    end
+  end
+
   get '/', to: 'users#index'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  
+
   get "/:username", to: 'users#datepicker' #for requester to browse to datepicker
   post "/:username", to: 'users#datepicker', as: 'email'
   post "/users/:id", to: 'users#show', as: 'showme' #lets integration form post to page for api call
